9fedc8f52ff5a84ca3c003430372fca281774456
joma0 <joanah.mayor@heig-vd.ch>
2024-11-14 11:46:32 +0100

    update

--- noSQL/src/views/noSQL.vue
+++ noSQL/src/views/noSQL.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="about">
     <h1>This is my page for noSQL</h1>
+    <h2>Nombre de post: {{ postsData.length }}</h2>
+    <ul> {{ postsData }}
+      <li v-for="post in postsData" :key="post._id">
+        <div class="ucfirst">{{ post.post_name }}<em style="font-size: x-small;"
+            v-if="post.attributes?.creation_date">
+            - {{ post.attributes?.creation_date }}
+
+            {{ post }}
+          </em>
+        </div>
+      </li>
+    </ul>
+    <h2>Ajouter un document</h2>
+    <button @click="createAndAddDemoDocument">Ajouter un document démo</button>
   </div>
 </template>
 
@@ -15,35 +29,128 @@
 </style>
 
 <script lang="ts">
+import {ref} from 'vue'; 
 import PouchDB from 'pouchdb'; 
+
+//structure d'un élément de la base de données
+declare interface Post {
+    _id: string,  
+    post_name: string,
+    post_content: string,
+    attributes: {
+      creation_date: string
+    }
+  
+}
 
 export default {
   data() {
     return {
-      database: null as PouchDB.Database | null,
+      //database: null as PouchDB.Database | null,
+      total: 0,
+      postsData: [] as Post[],
+      document: null as Post | null,
+      storage: null as PouchDB.Database | null,
     };
   },
 
   methods: {
     initDatabase() {
-        const db = new PouchDB('http://jm:pwd85@127.0.0.1:5986/blog_articles');
+        const db = new PouchDB('http://jm:pwd85@localhost:5984/post');
         if (db) {
-            console.log("fconnected to collection 'blog'");
+            console.log("connected to collection 'post'");
+            this.storage = db; 
         } else {
             console.warn("Something went wrong");
         }
-        //this.storage = db; 
     },
     
+    //récupérer les données depuis PouchDB
     fetchData() {
-        //db.allDocs();
+        const storage = ref(this.storage);
+        const self = this;
+        if (storage.value) {
+          (storage.value).allDocs({
+            include_docs: true,
+            attachments: true
+          }).then(function (result: any) {
+            console.log('fetchData success', result);
+            self.postsData = result.rows;
+          }.bind(this)).catch(function (error: any) {
+            console.log('fetchData error', error);
+        });
+      } else {
+        console.log('nothing in storage'); 
+      }
+    }, 
+
+    createAndAddDemoDocument(){
+      const document = {
+
+          post_name: "article_demo",
+          post_content: "Ceci est un article demo",
+          attributes: {
+            creation_date: "2024-09-09"
+          }  
+      }
+      this.addDocument(document);
+    },
+
+    addDocument (document: Omit<Post, '_id'>) {
+      if(this.storage) {
+        this.storage.post(document).then((result) => {
+          this.fetchData();
+        }).catch((error) => {
+          console.log('Failed to add document', error);
+        });
+        } else {
+          console.warn('Database not initialized'); 
+        }
+    },
+
+    updateDocument(id: string){
+      if(this.storage){
+        const storage = this.storage; 
+        storage.get(id).then((document: any) => {
+          document.post_content = "Ceci est le contenu modifié"; 
+          return storage.put(document); 
+        }).then(()=>{
+          console.log('Document updated successfully');
+        }).catch((err) => {
+          console.log('Failed to update document', err);
+        });        
+      } else {
+        console.warn('Database not initialized');
+      }
+    },
+
+    deleteDocument(id: string){
+      if(this.storage){
+        const storage = this.storage; 
+        storage.get(id).then((document: any) => {
+          return storage.remove(document);
+        }).then(() => {
+          console.log('Document removed successfully'); 
+        }).catch((err) => {
+          console.log('Failed to remove document', err)
+        })
+      } else {
+        console.warn('Database not initialized'); 
+      }
     }
 
   },
 
   mounted() {
     this.initDatabase()
+    this.fetchData()
+    //this.updateDocument("709f1efa0635ecea06646df075016536"); 
+    //this.deleteDocument("709f1efa0635ecea06646df07501e95d"); 
   }
 
 }
+
+
 </script>
+
+
